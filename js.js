70b0cde6c8f1f872c7945f7cca3dec00e412bc90
asadhossain <developerasad007@gmail.com>
2023-04-11 11:47:37 +0600

    ASAD

--- js.js
+++ js.js
@@ -1,2 +1,11 @@
-      document.write(22==22 && 44==44);
+// document.write(22==22 && 44==44);
+
+var avg = prompt("inter your point")
+
+if (avg >= 80 && avg<=100){
+    document.write("You Got A+")
+}
+    else if (avg >= 70 && avg <= 79){
+    document.write("You Got A")
+}
 
